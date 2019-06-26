cd147188e88df1df379c387d1d8639f72c0b4011
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-26 13:42:17 +0100

    Added logic for finding largest number in a sub array

--- js/app.js
+++ js/app.js
@@ -6,5 +6,27 @@ window.onload = function() {
 	// Edit me
 	let arr = [[10, 20, 30, 40], [16, 256, 4, 64], [25, 16, 9, 4], [27, 8, 125, 64]];
 
-	
+	function largest(array) {
+		let largest = [];
+
+		array.forEach(function(sub) {
+			// largest.push(Math.max(...sub));
+			
+			let subMax;
+
+			for (let i = 0; i < sub.length; i++) {
+				if (i === 0) {
+					subMax = sub[i];
+				} else if (sub[i] > subMax) {
+					subMax = sub[i];
+				}
+			}
+
+			largest.push(subMax);
+		});
+
+		// console.log(largest);
+	}
+
+	largest(arr);
 };

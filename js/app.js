a2ace0b821252cc7ff4a387753dfdf578487f12e
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-26 13:37:05 +0100

    Added array containing sub arrays with numbers

--- js/app.js
+++ js/app.js
@@ -2,4 +2,9 @@
 
 window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	// Edit me
+	let arr = [[10, 20, 30, 40], [16, 256, 4, 64], [25, 16, 9, 4], [27, 8, 125, 64]];
+
+	
 };

8cfed94cdad50397b08ae446b1a90c4fd6ae92c4
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-26 14:18:45 +0100

    Added logic for displaying largest numbers and number of arrays

--- js/app.js
+++ js/app.js
@@ -6,12 +6,27 @@ window.onload = function() {
 	// Edit me
 	let arr = [[10, 20, 30, 40], [16, 256, 4, 64], [25, 16, 9, 4], [27, 8, 125, 64]];
 
+	function display(largest) {
+		document.getElementById("arrays-count").textContent = largest.length;
+		document.getElementById("numbers-max").textContent = "";
+
+		for (let i = 0; i < largest.length; i++) {
+			if (i === (largest.length - 1)) {
+				document.getElementById("numbers-max").textContent = document.getElementById("numbers-max").textContent + largest[i];
+			} else {
+				document.getElementById("numbers-max").textContent = document.getElementById("numbers-max").textContent + largest[i] + ", ";
+			}
+		}
+	}
+
 	function largest(array) {
 		let largest = [];
 
 		array.forEach(function(sub) {
+			// Method I
 			// largest.push(Math.max(...sub));
-			
+
+			// Method II
 			let subMax;
 
 			for (let i = 0; i < sub.length; i++) {
@@ -25,7 +40,7 @@ window.onload = function() {
 			largest.push(subMax);
 		});
 
-		// console.log(largest);
+		display(largest);
 	}
 
 	largest(arr);
